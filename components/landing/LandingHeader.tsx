9418ffeb9587a5c83f4fc91f4ad2f7531494c187
Vxrcel <skyeparagon@gmail.com>
2026-07-18 00:02:20 +0100

    fix: increase z-index for landing header to improve visibility

--- components/landing/LandingHeader.tsx
+++ components/landing/LandingHeader.tsx
@@ -22,7 +22,7 @@ export default function LandingHeader() {
 	return (
 		<div
 			className={cn(
-				"fixed inset-x-0 top-0 z-50 flex items-center justify-between px-6 py-4 transition-colors duration-300 sm:pl-8",
+				"fixed inset-x-0 top-0 z-99 flex items-center justify-between px-6 py-4 transition-colors duration-300 sm:pl-8",
 				scrolled
 					? " bg-surface/70 dark:bg-surface/40 backdrop-blur-xl"
 					: "bg-transparent",
